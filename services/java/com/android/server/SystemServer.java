6da47ded234e10438235ea87938c56ba83e343e3
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-06-15 15:48:07 -0300

    Initial import of lineagehw
    All credits to LineageOS and related authors on:
    
    https://github.com/LineageOS/android_lineage-sdk
    
    Signed-off-by: Henrique Silva <jhenrique09.mcz@hotmail.com>
    Change-Id: Ief90ce8c20115d6a1c17c84a9874af6246dc1dcd
    Signed-off-by: Drenzzz <realdrenzzz@gmail.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -183,6 +183,7 @@ import com.android.server.inputmethod.InputMethodManagerService;
 import com.android.server.integrity.AppIntegrityManagerService;
 import com.android.server.job.JobSchedulerService;
 import com.android.server.lights.LightsService;
+import com.android.server.lineage.LineageHardwareService;
 import com.android.server.locales.LocaleManagerService;
 import com.android.server.location.LocationManagerService;
 import com.android.server.location.altitude.AltitudeService;
@@ -2746,6 +2747,10 @@ public final class SystemServer implements Dumpable {
             // Lineage Health
             t.traceBegin("StartHealthService");
             mSystemServiceManager.startService(HealthInterfaceService.class);
+
+            // LineageHardware
+            t.traceBegin("StartLineageHardwareService");
+            mSystemServiceManager.startService(LineageHardwareService.class);
             t.traceEnd();
 
             if (!com.android.server.flags.Flags.optionalBackgroundInstallControl()
