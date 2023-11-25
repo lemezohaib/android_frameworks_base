7f7098f2b554e6d1284e605868b1c8794810b584
Michael W <baddaemon87@gmail.com>
2023-11-25 14:29:19 +0100

    SystemUI: Internet tile: Allow long-pressing on mobile network
    * The current WiFi network has a setting toggle and also opens the
      settings on single click where the mobile network only allows
      connecting but not opening settings
    * Add a long press listener that goes to the subId's network settings,
      allowing for example to disable roaming or change network
    
    Change-Id: Iacc1c64d318b0240da5397f2b4e4dfb59d4834dd

--- packages/SystemUI/src/com/android/systemui/qs/tiles/dialog/InternetDialogDelegate.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/dialog/InternetDialogDelegate.java
@@ -342,6 +342,7 @@ public class InternetDialogDelegate implements
         }
         mLifecycleRegistry.setCurrentState(Lifecycle.State.DESTROYED);
         mMobileNetworkLayout.setOnClickListener(null);
+        mMobileNetworkLayout.setOnLongClickListener(null);
         mConnectedWifListLayout.setOnClickListener(null);
         if (mSecondaryMobileNetworkLayout != null) {
             mSecondaryMobileNetworkLayout.setOnClickListener(null);
@@ -453,6 +454,13 @@ public class InternetDialogDelegate implements
                 mInternetDialogController.setMobileDataEnabled(
                         dialog.getContext(), mDefaultDataSubId, isChecked, false);
             }
+        });
+        mMobileNetworkLayout.setOnLongClickListener(v -> {
+            if (!mInternetDialogController.isDeviceLocked()) {
+                mInternetDialogController.launchMobileNetworkSettings(v, mDefaultDataSubId);
+                return true;
+            }
+            return false;
         });
         mConnectedWifListLayout.setOnClickListener(this::onClickConnectedWifi);
         mSeeAllLayout.setOnClickListener(this::onClickSeeMoreButton);
