b9e7999000e0fe3915dcecefc3df0e916f019bf1
someone5678 <someone5678.dev@gmail.com>
2024-12-29 14:34:06 +0900

    fixup! base: Implement cutout force full screen
    Change-Id: Icbfb3de50fba46d76077319491c3814bddb457bf

--- services/core/java/com/android/server/wm/ActivityRecord.java
+++ services/core/java/com/android/server/wm/ActivityRecord.java
@@ -10635,4 +10635,8 @@ final class ActivityRecord extends WindowToken implements WindowManagerService.A
                     mTaskDescription, mCreateTime);
         }
     }
+
+    public boolean shouldForceCutoutFullscreen() {
+        return mAtmService.shouldForceCutoutFullscreen(packageName);
+    }
 }
