f8d62c92388eb1926f727c2a5329ce2a865e0285
lemezohaib <zhbkhan644@gmail.com>
2025-03-02 10:21:15 +0100

    Revert "pm: aconfig: do not error on flags missing per partition"
    This reverts commit 0ce479eb2c96b06b2498975d33e83611d53f4322.

--- core/java/com/android/internal/pm/pkg/component/AconfigFlags.java
+++ core/java/com/android/internal/pm/pkg/component/AconfigFlags.java
@@ -71,15 +71,10 @@ public class AconfigFlags {
                 (Process.myUid() == Process.SYSTEM_UID) ? DeviceProtos.parsedFlagsProtoPaths()
                         : Arrays.asList(DeviceProtos.PATHS);
         for (String fileName : defaultFlagProtoFiles) {
-            File f = new File(fileName);
-            if (f.isFile() && f.canRead()) {
-                try (FileInputStream inputStream = new FileInputStream(fileName)) {
-                    loadAconfigDefaultValues(inputStream.readAllBytes());
-                } catch (IOException e) {
-                    Slog.e(LOG_TAG, "Failed to read Aconfig values from " + fileName, e);
-                }
-            } else {
-                Slog.w(LOG_TAG, "No Aconfig flags at " + fileName);
+            try (var inputStream = new FileInputStream(fileName)) {
+                loadAconfigDefaultValues(inputStream.readAllBytes());
+            } catch (IOException e) {
+                Slog.e(LOG_TAG, "Failed to read Aconfig values from " + fileName, e);
             }
         }
         if (Process.myUid() == Process.SYSTEM_UID) {
