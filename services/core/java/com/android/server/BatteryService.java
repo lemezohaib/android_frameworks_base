02972e2991455bb946fc720536d86e41e1556e3a
COSMIC <rayavanindra@gmail.com>
2025-03-15 13:49:23 +0530

    Revert "core: BatteryManager: Add few more Battery broadcast intents"
    This reverts commit 107ddb1c17a26b8b90b0255320f8255c403b9c14.

--- services/core/java/com/android/server/BatteryService.java
+++ services/core/java/com/android/server/BatteryService.java
@@ -263,20 +263,6 @@ public final class BatteryService extends SystemService {
      */
     private int mLastBroadcastInvalidCharger;
     /**
-     * {@link #mBatteryFullCharge} value when {@link Intent#ACTION_BATTERY_CHANGED}
-     * broadcast was sent last.
-     * Note: These values may be used for internal operations and/or to determine whether to trigger
-     * the broadcast or not.
-     */
-    private int mLastBroadcastBatteryFullCharge;
-    /**
-     * {@link #mBatteryFullChargeDesign} value when {@link Intent#ACTION_BATTERY_CHANGED}
-     * broadcast was sent last.
-     * Note: These values may be used for internal operations and/or to determine whether to trigger
-     * the broadcast or not.
-     */
-    private int mLastBroadcastBatteryFullChargeDesign;
-    /**
      * The last seen charging policy. This requires the
      * {@link android.Manifest.permission#BATTERY_STATS} permission and should therefore not be
      * included in the ACTION_BATTERY_CHANGED intent extras.
@@ -784,8 +770,6 @@ public final class BatteryService extends SystemService {
                 || mHealthInfo.maxChargingVoltageMicrovolts != mLastBroadcastMaxChargingVoltage
                 || includeChargeCounter
                 || mInvalidCharger != mLastBroadcastInvalidCharger
-                || mHealthInfo.batteryFullChargeUah != mLastBroadcastBatteryFullCharge
-                || mHealthInfo.batteryFullChargeDesignCapacityUah != mLastBroadcastBatteryFullChargeDesign
                 || mHealthInfo.batteryCycleCount != mLastBroadcastBatteryCycleCount
                 || mHealthInfo.chargingState != mLastBroadcastChargingState
                 || mHealthInfo.batteryCapacityLevel != mLastBroadcastBatteryCapacityLevel
@@ -1002,7 +986,6 @@ public final class BatteryService extends SystemService {
                 logOutlierLocked(dischargeDuration);
             }
 
-<<<<<<< HEAD
             // Only update the values when we send the broadcast
             if (!rateLimitBatteryChangedBroadcast) {
                 mLastBroadcastBatteryStatus = mHealthInfo.batteryStatus;
@@ -1020,8 +1003,6 @@ public final class BatteryService extends SystemService {
                 mLastBroadcastBatteryCycleCount = mHealthInfo.batteryCycleCount;
                 mLastBroadcastChargingState = mHealthInfo.chargingState;
                 mLastBroadcastBatteryCapacityLevel = mHealthInfo.batteryCapacityLevel;
-                mLastBroadcastBatteryFullCharge = mHealthInfo.batteryFullChargeUah;
-                mLastBroadcastBatteryFullChargeDesign = mHealthInfo.batteryFullChargeDesignCapacityUah;
                 mLastOemFastCharging = mOemFastCharging;
             }
         }
@@ -1058,8 +1039,6 @@ public final class BatteryService extends SystemService {
         intent.putExtra(BatteryManager.EXTRA_CYCLE_COUNT, mHealthInfo.batteryCycleCount);
         intent.putExtra(BatteryManager.EXTRA_CHARGING_STATUS, mHealthInfo.chargingState);
         intent.putExtra(BatteryManager.EXTRA_CAPACITY_LEVEL, mHealthInfo.batteryCapacityLevel);
-        intent.putExtra(BatteryManager.EXTRA_MAXIMUM_CAPACITY, mHealthInfo.batteryFullChargeUah);
-        intent.putExtra(BatteryManager.EXTRA_DESIGN_CAPACITY, mHealthInfo.batteryFullChargeDesignCapacityUah);
         intent.putExtra(BatteryManager.EXTRA_OEM_FAST_CHARGING, mOemFastCharging);
         if (DEBUG) {
             Slog.d(TAG, "Sending ACTION_BATTERY_CHANGED. scale:" + BATTERY_SCALE
@@ -1165,8 +1144,6 @@ public final class BatteryService extends SystemService {
         event.putInt(BatteryManager.EXTRA_CYCLE_COUNT, mHealthInfo.batteryCycleCount);
         event.putInt(BatteryManager.EXTRA_CHARGING_STATUS, mHealthInfo.chargingState);
         event.putInt(BatteryManager.EXTRA_CAPACITY_LEVEL, mHealthInfo.batteryCapacityLevel);
-        event.putInt(BatteryManager.EXTRA_MAXIMUM_CAPACITY, mHealthInfo.batteryFullChargeUah);
-        event.putInt(BatteryManager.EXTRA_DESIGN_CAPACITY, mHealthInfo.batteryFullChargeDesignCapacityUah);
 
         boolean queueWasEmpty = mBatteryLevelsEventQueue.isEmpty();
         mBatteryLevelsEventQueue.add(event);
